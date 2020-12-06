d24cdebe8d1ca15c78ae7bbc4baa9e2d305ce9dd
Cole Mickens <cole.mickens@gmail.com>
2020-12-05 20:09:25 -0800

    change more?

--- queue.go
+++ queue.go
@@ -17,21 +17,40 @@ func processUploadQueue(c *nicheClient, queue chan string, wg *sync.WaitGroup, a
 
 	seenPaths := []string{}
 
+outer:
 	for storePath := range queue {
+		// if we've seen this path, skip it
 		if storePath == "QUIT" {
 			log.Info().Msg("leaving build queue")
 			return
 		}
+		// if we've seen this path, skip it, continue the otuer loop
 		for _, seenPath := range seenPaths {
 			if strings.EqualFold(storePath, seenPath) {
-				continue
+				continue outer
 			}
 		}
-		if alwaysOverwrite {
-			c.uploadPath(storePath)
-		} else {
-			c.ensurePath(storePath)
+
+		allStorePaths, err := nix.QueryPaths(storePath)
+		if err != nil {
+			log.Warn().Err(err).Msg("unexpected error querying all store paths")
+			break
 		}
-		seenPaths = append(seenPaths, storePath)
+
+	inner:
+		for _, innerStorePath := range allStorePaths {
+			// if we've seen this path, skip it, continue to the next "innerStorePath"
+			for _, seenPath := range seenPaths {
+				if strings.EqualFold(innerStorePath, seenPath) {
+					continue inner
+				}
+			}
+			if alwaysOverwrite {
+				c.uploadPath(innerStorePath)
+			} else {
+				c.ensurePath(innerStorePath)
+			}
+			seenPaths = append(seenPaths, storePath)
+		}
 	}
 }
