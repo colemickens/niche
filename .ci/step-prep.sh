65c7536a8558aea0d6880192b34f0b632568fd14
Cole Mickens <cole.mickens@gmail.com>
2020-12-14 16:56:35 -0800

    ci

--- .ci/step-prep.sh
+++ .ci/step-prep.sh
@@ -4,7 +4,10 @@ set -x
 
 cachix use colemickens
 
+# not sure this is needed with https git remote
+mkdir -p "${HOME}/.ssh"
 ssh-keyscan github.com >> ${HOME}/.ssh/known_hosts
+# would be nice to have this the same across srht jobs
 
 git config --global user.name \
  "Cole Botkens"
