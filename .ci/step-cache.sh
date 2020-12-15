73c9884482d5101fe9068b5adc31359eda7ce28a
Cole Mickens <cole.mickens@gmail.com>
2020-12-14 16:31:26 -0800

    ci

--- .ci/step-cache.sh
+++ .ci/step-cache.sh
@@ -4,6 +4,8 @@ set -x
 
 cache="niche"
 
+set +x; export CACHE_SIGNING_KEY="$(cat '.ci/unencrypted/cachix_niche_signing_key' | head -1)"; set -x
+
 cd ..
 nix build .
 readlink -f result | cachix push "${cache}"
--- .ci/step-push.sh
+++ .ci/step-push.sh
@@ -4,6 +4,8 @@ set -x
 
 ssh-keyscan github.com >> ${HOME}/.ssh/known_hosts
 
+# TODO: load GitHub PAT from unencrypted secret
+
 git add -A .
 git diff-index --cached --quiet HEAD || git commit -m "auto-updates"
 git push origin HEAD
