2f5f4dc9fb9fe78258ec88167c7c154974ce82a5
Cole Mickens <cole.mickens@gmail.com>
2020-12-14 18:23:11 -0800

    ci

--- tests/test-s3.sh
+++ tests/test-s3.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 set -euo pipefail
 set -x
 
-(cd ..; go build .)
+(cd "${DIR}/.."; go build .); cd "${DIR}"
 
 export thing="github:colemickens/flake-impure"
 export cachename="cache${RANDOM}"
