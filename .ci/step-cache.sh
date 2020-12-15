429aa3f87efefa87d93fb5b83c993d8466ede742
Cole Mickens <cole.mickens@gmail.com>
2020-12-14 17:56:46 -0800

    ci

--- .ci/step-cache.sh
+++ .ci/step-cache.sh
@@ -4,7 +4,9 @@ set -x
 
 cache="niche"
 
-set +x; export CACHE_SIGNING_KEY="$(cat '.ci/unencrypted/cachix_niche_signing_key' | head -1)"; set -x
+#set +x;
+export CACHE_SIGNING_KEY="$(cat '.ci/unencrypted/cachix_niche_signing_key' | head -1)"
+#set -x
 
 nix --experimental-features 'nix-command flakes' build .
 readlink -f result | cachix push "${cache}"
